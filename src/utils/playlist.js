55c455919c4f8cd6c9e833ef16852b0a5f4d51e1
Abhi Kumar <abhi_yallapu@tecnics.com>
2024-12-16 17:14:38 +0530

    add utils folder and playlist api fetch functions dummy definitions

---
+++ src/utils/playlist.js
@@ -0,0 +1,22 @@
+function getAllPlaylists() {
+
+}
+
+function getPlaylistById(playlistId) {
+
+}
+
+function createPlaylist(playlistInput) {
+
+}
+
+function updatePlaylist(playlistId, playlistUpdates) {
+
+}
+
+export {
+    getAllPlaylists,
+    getPlaylistById,
+    createPlaylist,
+    updatePlaylist
+};
